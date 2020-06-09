c6691efb78b69d3c360a7482175c30e3b17747b0
selinalmolhem <zak@DESKTOP-KKD17F7>
2020-06-09 21:07:49 +0300

    DBConnection dosyası  Eklendi

---
+++ src/java/login_logout/DBConnection.java
@@ -0,0 +1,34 @@
+package login_logout;
+
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class DBConnection {
+   
+	public static Connection getConnection() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con = DriverManager.getConnection(
+					"jdbc:mysql://localhost:3306/otel12?user=root&password=123");
+			return con;
+		} catch (Exception ex) {
+			System.out.println("Database.getConnection() Error -->"
+					+ ex.getMessage());
+			return null;
+		}
+	}
+
+	public static void close(Connection con) {
+		try {
+			con.close();
+		} catch (Exception ex) {
+		}
+	}
+
+}
