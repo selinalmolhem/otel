ebbb88fa7b4d48653ae6948e1d32a07d05b08d26
selinalmolhem <zak@DESKTOP-KKD17F7>
2020-06-09 21:08:42 +0300

    SessionUtils dosyası  Eklendi

---
+++ src/java/login_logout/SessionUtils.java
@@ -0,0 +1,48 @@
+package login_logout;
+
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+public class SessionUtils {
+
+    public static HttpSession getSession() {
+        return (HttpSession) FacesContext.getCurrentInstance()
+                .getExternalContext().getSession(false);
+    }
+
+    public static HttpServletRequest getRequest() {
+        return (HttpServletRequest) FacesContext.getCurrentInstance()
+                .getExternalContext().getRequest();
+    }
+
+    public static String getUserName() {
+        HttpSession session = (HttpSession) FacesContext.getCurrentInstance()
+                .getExternalContext().getSession(false);
+        return session.getAttribute("username").toString();
+    }
+
+    public static String userTuru() {
+        HttpSession session = (HttpSession) FacesContext.getCurrentInstance()
+                .getExternalContext().getSession(false);
+        return session.getAttribute("userTuru").toString();
+    }
+
+    public static String getUserId() {
+        HttpSession session = getSession();
+        if (session != null) {
+            return (String) session.getAttribute("userid");
+        } else {
+            return null;
+        }
+    }
+    public static boolean getC() {
+        HttpSession session = getSession();
+        if (session != null) {
+            return (Boolean) session.getAttribute("C");
+        } else {
+            return false;
+        }
+    }
+
+}
